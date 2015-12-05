2198d60a63c2d9681331b4932c81082e436ea5f6
Wittakarn Keeratichayakorn <wittakarn.expert@gmail.com>
2015-12-05 09:40:22 +0700

    Use modal instead of popover to show a figure.

--- include/science-degree-1.php
+++ include/science-degree-1.php
@@ -1,3 +1,28 @@
-<p>
-science 1
-</p>
+<?php
+ini_set('display_errors', 1); 
+error_reporting(E_ALL);
+require_once("../config.php");
+?>
+<div class="row">
+	<div class="col-md-3">
+		<a href="#" ref=popover class="thumbnail" data-toggle="modal" data-target=".m-s-1-1">
+			<img src="<?php echo ROOT;?>image/nuton.jpg" class="img-responsive"/>
+		</a>
+	</div>
+	<div class="modal fade m-s-1-1" tabindex="-1" role="dialog">
+		<div class="modal-dialog modal-lg" role="document">
+			<div class="modal-content">
+				<div class="modal-body">
+					<button type="button" 
+							class="close" 
+							data-dismiss="modal" 
+							aria-label="Close">
+						<span class="glyphicon glyphicon-remove" aria-hidden="true">
+						</span>
+					</button>
+					<img src="<?php echo ROOT;?>image/nuton.jpg" class="img-responsive"/>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
