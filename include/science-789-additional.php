1c981adf892aec705eb88142ae6ba510b89e625d
Wittakarn Keeratichayakorn <wittakarn.expert@gmail.com>
2015-12-25 21:39:00 +0700

    Improve science book 7-9.

---
+++ include/science-789-additional.php
@@ -0,0 +1,23 @@
+<?php
+ini_set('display_errors', 1); 
+error_reporting(E_ALL);
+require_once("../config.php");
+?>
+
+<h4><u>วิทยาศาสตร์ รายวิชาเพิ่มเติม</u></h4>
+<?php 
+	$bookCount = 5;
+	$bookNames = array('หนังสือเรียน เชื้อเพลิงเพื่อการคมนาคม', 'หนังสือเรียน ของเล่นเชิงวิทยาศาสตร์', 'หนังสือเรียน วิทยาศาสตร์กับความงาม', 'หนังสือเรียน สนุกกับโครงงานวิทยาศาสตร์', 'หนังสือเรียน พลังงานทดแทนกับการใช้ประโยชน์');
+	for($i=1;$i<=$bookCount;$i++)
+	{
+		$modelClass = 'm-s-789-additional-'.$i;
+		$imagePath = 'image/s/789-additional/'.$i.'.jpg';
+		$bookName = $bookNames[($i - 1)];  /* --- ใช้ ($i-1) เพราะว่า array เริ่มต้นด้วย index ที่ 0 --- */
+		$nameOfComponent = array('r_s_789_additional_'.$i.'_1', 'r_s_789_additional_'.$i.'_2', 't_s_789_additional_'.$i.'_3', 'f_s_789_additional_'.$i.'_4', 'h_s_789_additional_'.$i.'_4');
+		$collapseSectionId = 'ms789Additional'.$i.'SelectedCollapse';
+		$fileUploadId = 'fs789Additional'.$i.'4';
+		
+		include DOCUMENT_ROOT.'include/book-satisfaction-template.php';
+		echo '<hr/>';
+	}
+?>
