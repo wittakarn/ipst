b41720d32e4bfa7d809fa56e3714244470c26839
Nutonninizz <capzoon_nil@yahoo.com>
2015-12-30 23:21:20 +0700

    adding if isset in math 3-9

--- include/math-6.php
+++ include/math-6.php
@@ -21,4 +21,21 @@ require_once("../config.php");
 		include DOCUMENT_ROOT.'include/book-satisfaction-template.php';
 		echo '<hr/>';
 	}
+	
+	if(isset($_REQUEST['type']) && $_REQUEST['type'] === 't'){
+		$bookCount = 1;
+		$bookNames = array('คู่มือครู');
+		for($i=1;$i<=$bookCount;$i++)
+		{
+			$modelClass = 'm-m-ins-6-'.$i;
+			$imagePath = 'image/m/instructor/6/'.$i.'.jpg';
+			$bookName = $bookNames[($i - 1)];  /* --- ใช้ ($i-1) เพราะว่า array เริ่มต้นด้วย index ที่ 0 --- */
+			$nameOfComponent = array('r_m_ins_6_'.$i.'_1', 'r_m_ins_6_'.$i.'_2', 't_m_ins_6_'.$i.'_3', 'f_m_ins_6_'.$i.'_4', 'h_m_ins_6_'.$i.'_4');
+			$collapseSectionId = 'mMIns6'.$i.'SelectedCollapse';
+			$fileUploadId = 'fMIns6'.$i.'4';
+			
+			include DOCUMENT_ROOT.'include/book-satisfaction-template.php';
+			echo '<hr/>';
+		}
+	}
 ?>
--- include/math-8.php
+++ include/math-8.php
@@ -21,4 +21,21 @@ require_once("../config.php");
 		include DOCUMENT_ROOT.'include/book-satisfaction-template.php';
 		echo '<hr/>';
 	}
+	
+	if(isset($_REQUEST['type']) && $_REQUEST['type'] === 't'){
+		$bookCount = 4;
+		$bookNames = array('คู่มือครู รายวิชาพื้นฐาน เล่ม 1', 'คู่มือครู รายวิชาพื้นฐาน เล่ม 2', 'คู่มือครู รายวิชาเพิ่มเติม เล่ม 1', 'คู่มือครู รายวิชาเพิ่มเติม เล่ม 2');
+		for($i=1;$i<=$bookCount;$i++)
+		{
+			$modelClass = 'm-m-ins-8-'.$i;
+			$imagePath = 'image/m/instructor/8/'.$i.'.jpg';
+			$bookName = $bookNames[($i - 1)];  /* --- ใช้ ($i-1) เพราะว่า array เริ่มต้นด้วย index ที่ 0 --- */
+			$nameOfComponent = array('r_m_ins_8_'.$i.'_1', 'r_m_ins_8_'.$i.'_2', 't_m_ins_8_'.$i.'_3', 'f_m_ins_8_'.$i.'_4', 'h_m_ins_8_'.$i.'_4');
+			$collapseSectionId = 'mMIns8'.$i.'SelectedCollapse';
+			$fileUploadId = 'fMIns8'.$i.'4';
+			
+			include DOCUMENT_ROOT.'include/book-satisfaction-template.php';
+			echo '<hr/>';
+		}
+	}
 ?>
